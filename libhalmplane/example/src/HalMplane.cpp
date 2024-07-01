45a1f3d315539272cc0c20c4ba741bd6932a398e
akram <akram@cloudly.io>
2024-07-01 05:27:07 +0000

    Create shared lib with Makefile

--- libhalmplane/example/src/HalMplane.cpp
+++ libhalmplane/example/src/HalMplane.cpp
@@ -4,11 +4,12 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
+#include <iostream>
 #include "HalMplane.h"
 
 int halmplane_init()
 {
+    std::cout << "Halmplane_init()" << std::endl;
     return 0;
 }
 
